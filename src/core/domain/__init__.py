b7c4f76e7e13a473c665f5fb71a0ae7c1cea5bc1
Nikita Ugryumov <nvugryumov@edu.hse.ru>
2025-12-13 23:22:01 +0300

    add domain

---
+++ src/core/domain/__init__.py
@@ -0,0 +1,17 @@
+from .entities import (
+    User,
+    UserRole,
+    Document,
+    DocumentStatus,
+    ChatMessage,
+    SearchResult,
+)
+
+__all__ = [
+    "User",
+    "UserRole",
+    "Document",
+    "DocumentStatus",
+    "ChatMessage",
+    "SearchResult",
+]
