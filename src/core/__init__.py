ada253657b5c2ea4dace144fe1a640f343963dec
Nikita Ugryumov <nvugryumov@edu.hse.ru>
2025-12-14 18:01:19 +0300

    refactor code

---
+++ src/core/__init__.py
@@ -0,0 +1,8 @@
+# Core services
+from .services import RAGService, IngestionService, DocumentGenerationService
+
+__all__ = [
+    "RAGService",
+    "IngestionService",
+    "DocumentGenerationService",
+]
